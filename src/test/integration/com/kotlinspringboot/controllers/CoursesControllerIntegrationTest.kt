b27dd5cbb242941ea4433ff6d032c3b1588bd10d
Eugene Oei <eugene.oei@thoughtworks.com>
2022-08-02 17:44:35 +0800

    added integration test for CoursesController

---
+++ src/test/integration/com/kotlinspringboot/controllers/CoursesControllerIntegrationTest.kt
@@ -0,0 +1,39 @@
+package com.kotlinspringboot.controllers
+
+import com.kotlinspringboot.dto.CourseDto
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CoursesControllerIntegrationTest {
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun create() {
+        val course = CourseDto(null, "my course", "my course category")
+        val expected = CourseDto(1, "my course", "my course category")
+
+        val result = webTestClient
+            .post()
+            .uri("/courses")
+            .bodyValue(course)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            expected == result
+        }
+    }
+
+}
